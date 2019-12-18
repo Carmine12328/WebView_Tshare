fdb363aa91ff971c078cb3bed966282a98270bee
Carmine12328 <carmine12328@gmail.com>
2019-12-18 14:03:36 +0100

    Added function that permit to enable a button do download files on webView

--- Tshare/app/src/main/java/com/example/tshare/MainActivity.java
+++ Tshare/app/src/main/java/com/example/tshare/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+        }
+        });
     }
 
     @Override
@@ -33,4 +45,8 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+
+
 }
+
